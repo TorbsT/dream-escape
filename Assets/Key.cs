e16758e7cd33ef6f9f114ce3de131d823ffd1561
TorbsT <torbst2001@gmail.com>
2023-10-07 14:51:29 +0200

    change to laptop

---
+++ Assets/Key.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Key : MonoBehaviour
+{
+    [field: SerializeField] public List<TriggerReceiver> Receivers { get; private set; } = new();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (var item in Receivers)
+        {
+            if (item == null) continue;
+            Gizmos.DrawLine(transform.position, item.transform.position);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (var item in Receivers)
+            item.Trigger();
+        gameObject.SetActive(false);
+        Invoke(nameof(Reactivate), 1.5f);
+    }
+    private void Reactivate()
+    {
+        gameObject.SetActive(true);
+    }
+}
