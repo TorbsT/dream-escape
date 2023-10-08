db2d9bc71f0a71c6c297a9b89d6d82e79c77a6f0
TorbsT <torbst2001@gmail.com>
2023-10-08 18:17:30 +0200

    restart text

--- Assets/DimensionComponent.cs
+++ Assets/DimensionComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteAlways]
+[SelectionBase]
 public class DimensionComponent : MonoBehaviour
 {
     [field: SerializeField] public bool RED { get; set; } = true;
---
+++ Assets/ShowUnderConditions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowUnderConditions : TriggerReceiver
+{
+    [field: SerializeField] public List<GameObject> Conditions { get; private set; } = new();
+    private void Awake()
+    {
+        SetEnabled(false);
+    }
+    public override void Trigger()
+    {
+        foreach (var condition in Conditions)
+        {
+            if (!condition.activeSelf) return;
+        }
+        SetEnabled(true);
+    }
+    private void SetEnabled(bool enabled)
+    {
+        GetComponent<TextMeshProUGUI>().enabled = enabled;
+    }
+}
